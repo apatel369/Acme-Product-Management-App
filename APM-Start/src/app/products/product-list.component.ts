3bfbe669b9a9e86e5a890bfe7e1dd878f4f081ab
Arpan Patel <175540@ustexaus613005.dot.state.tx.us>
2019-02-28 07:02:40 -0600

    show hide images in row

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import {Component} from '@angular/core';
 
 export class ProductListComponent {
  pageTitle: string = 'Product List';
+ imageWidth: number = 50;
+ imageMargin: number = 2;
+ showImage: Boolean = false;
  products: any[] = [
   {
     "productId": 1,
@@ -58,7 +61,12 @@ export class ProductListComponent {
     "starRating": 4.6,
     "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
   }
-]
+  ];
+
+toggleImage(): void{
+    this.showImage = !this.showImage;
+}
+
 
 
 }
