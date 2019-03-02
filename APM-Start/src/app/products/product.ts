cfd6b8a4ea061c225907331dbfcad7e63343e40c
Arpan Patel <175540@ustexaus613005.dot.state.tx.us>
2019-03-02 17:57:03 -0600

    implement product type interface and add style for table headers

---
+++ APM-Start/src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface Iproduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
