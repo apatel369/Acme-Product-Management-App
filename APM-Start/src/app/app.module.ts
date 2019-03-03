41338330d1ff333d64dece9afd9a185a39531aef
Arpan Patel <175540@ustexaus613005.dot.state.tx.us>
2019-03-02 18:57:24 -0600

    create and apply convertToSpacePipe to product code

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import {ConvertToSpacePipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe
   ],
   imports: [
     BrowserModule,
